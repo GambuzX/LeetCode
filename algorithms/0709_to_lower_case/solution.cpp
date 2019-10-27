eeb8a6014432579578e12c5349514cb73ea524e7
GambuzX <mg_mesquita@hotmail.com>
2019-10-27 17:24:03 +0000

    To lower case

---
+++ algorithms/0709_to_lower_case/solution.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    string toLowerCase(string str) {
+        string newString = "";
+        
+        for (char & c : str) {
+            if (c >= 65 && c <= 90)
+                newString += c+32;
+            else
+                newString += c;
+        }
+        
+        return newString;
+    }
+};
+
+string stringToString(string input) {
+    assert(input.length() >= 2);
+    string result;
+    for (int i = 1; i < input.length() -1; i++) {
+        char currentChar = input[i];
+        if (input[i] == '\\') {
+            char nextChar = input[i+1];
+            switch (nextChar) {
+                case '\"': result.push_back('\"'); break;
+                case '/' : result.push_back('/'); break;
+                case '\\': result.push_back('\\'); break;
+                case 'b' : result.push_back('\b'); break;
+                case 'f' : result.push_back('\f'); break;
+                case 'r' : result.push_back('\r'); break;
+                case 'n' : result.push_back('\n'); break;
+                case 't' : result.push_back('\t'); break;
+                default: break;
+            }
+            i++;
+        } else {
+          result.push_back(currentChar);
+        }
+    }
+    return result;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        string str = stringToString(line);
+        
+        string ret = Solution().toLowerCase(str);
+
+        string out = (ret);
+        cout << out << endl;
+    }
+    return 0;
+}
