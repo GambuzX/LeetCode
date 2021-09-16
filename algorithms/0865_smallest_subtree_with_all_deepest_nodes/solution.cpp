b1cbe74cf21199a1b04346be60fafc2d76eecd39
GambuzX <mg_mesquita@hotmail.com>
2021-09-16 12:44:40 +0100

    Lowest common ancestor of deepest leaves

---
+++ algorithms/0865_smallest_subtree_with_all_deepest_nodes/solution.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* findMaxDepth(TreeNode* head, int depth, unordered_map<TreeNode*,int> &nodesMaxDepth) {
+        if (head == nullptr) return nullptr;
+        
+        nodesMaxDepth[head] = depth;
+        
+        TreeNode* left = findMaxDepth(head->left, depth+1, nodesMaxDepth);
+        TreeNode* right = findMaxDepth(head->right, depth+1, nodesMaxDepth);
+        if (left == nullptr) left = head;
+        if (right == nullptr) right = head;
+        
+        int leftMaxDepth = nodesMaxDepth[left];
+        int rightMaxDepth = nodesMaxDepth[right];
+        nodesMaxDepth[head] = max(leftMaxDepth, rightMaxDepth);
+        
+        if (leftMaxDepth == rightMaxDepth) return head;
+        else if (leftMaxDepth > rightMaxDepth) return left;
+        else return right;
+    }
+    
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        unordered_map<TreeNode*,int> nodesMaxDepth;
+        return findMaxDepth(root, 0, nodesMaxDepth);
+    }
+};
