6c8c024595d9ab0e2969dda2f0a1ce43c7218ff5
GambuzX <mg_mesquita@hotmail.com>
2022-10-22 18:14:39 +0100

    169. Majority Element

---
+++ algorithms/0169_majority_element/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> count;
+        for (int n : nums) {
+            count[n]++;
+            if (count[n] > nums.size()/2) return n;
+        }
+        return -1;
+    }
+};
